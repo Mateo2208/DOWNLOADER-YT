1babfdd63b18e57fabccd50f2be88c826c6e92e5
Mateo2208 <123609083+Mateo2208@users.noreply.github.com>
2024-06-26 01:36:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def descargar():
         return render_template('result.html', error=str(e))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
